b567c9498beea39f7b6fe33fef2a9d3fed3ec6c2
cfchou <cfchou@gmail.com>
2014-02-24 16:37:22 +0800

    add not-optimised merge sort

--- src/main/scala/cf/algs/MergeSort.scala
+++ src/main/scala/cf/algs/MergeSort.scala
@@ -31,8 +31,8 @@ class MergeSort {
     sortWith(arr, lo, hi, aux)
   }
 
-  private[this] def sortWith[T: OrderedLike](arr: IndexedSeq[T], lo: Int, hi: Int,
-                               aux: Array[T]): Unit = {
+  private[this] def sortWith[T: OrderedLike](arr: IndexedSeq[T], lo: Int,
+                                             hi: Int, aux: Array[T]): Unit = {
     // Optimization 1: cutoff to insertion sort
     if ((hi - lo) < 7) {
       val isort = new InsertionSort {}
@@ -46,17 +46,17 @@ class MergeSort {
   }
 
   def sortBottomUp[T](arr: IndexedSeq[T], lo: Int, hi: Int)
-             (implicit od: OrderedLike[T], tg: ClassTag[T]): Unit = {
-    require(lo <= hi && hi < arr.length)
-    val aux = Array.ofDim[T](arr.length)
+                     (implicit od: OrderedLike[T], tg: ClassTag[T]): Unit = {
+    val aux = Array.ofDim(arr.length)
     val len = hi - lo + 1
-    var step = 1
+
+    var step = 1 // @step can be seen as # of elements have been merged
     while (step < len) {
-      for (i <- (lo until hi by 2 * step)) {
-        merge(arr, i, i + step - 1, math.min(i + step + step - 1, hi),
-          aux)
+      val twoSteps = step + step
+      (lo to hi by step + step) foreach { l =>
+        merge(arr, l, l + step - 1, math.min(l + twoSteps - 1, hi), aux)
       }
-      step = step + step
+      step = twoSteps
     }
   }
 
@@ -92,4 +92,60 @@ class MergeSort {
       }
     }
   }
+
+
+  object NotOptimized {
+
+    def sort[T](arr: IndexedSeq[T])
+               (implicit od: OrderedLike[T], tg: ClassTag[T]): Unit = {
+      sort(arr, 0, arr.length - 1)
+    }
+
+    def sort[T](arr: IndexedSeq[T], lo: Int, hi: Int)
+               (implicit od: OrderedLike[T], tg: ClassTag[T]): Unit = {
+      if (lo < hi) {
+        val aux = Array.ofDim(arr.length)
+        sort(arr, lo, hi, aux)
+      }
+    }
+
+    def sort[T](arr: IndexedSeq[T], lo: Int, hi: Int, aux: Array[T])
+               (implicit od: OrderedLike[T], tg: ClassTag[T]): Unit = {
+      val mid = (lo + hi) / 2
+      if (lo < mid) {
+        sort(arr, lo, mid, aux)
+      }
+      if (mid + 1 < hi) {
+        sort(arr, mid + 1, hi, aux)
+      }
+      merge(arr, lo, mid, hi, aux)
+    }
+
+    def merge[T](arr: IndexedSeq[T], lo: Int, mid: Int, hi: Int,
+                 aux: Array[T])(implicit od: OrderedLike[T]): Unit = {
+      var p = lo
+      var q = mid + 1
+      (lo to hi) foreach { i =>
+        if (p > mid) {
+          aux(i) = arr(q)
+          q = q + 1
+        } else if (q > hi) {
+          aux(i) = arr(p)
+          p = p + 1
+        } else {
+          if (od.compare(arr(p), arr(q)) < 0) {
+            aux(i) = arr(p)
+            p = p + 1
+          } else {
+            aux(i) = arr(q)
+            q = q + 1
+          }
+        }
+      }
+      (lo to hi) foreach { i =>
+        arr(i) = aux(i)
+      }
+    }
+  }
 }
+
